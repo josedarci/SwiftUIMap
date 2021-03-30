923d4777dd3f4f53863c54f8bb6d386647b01e57
José Darci Rodrigues Junior <juniorinternet@gmail.com>
2021-03-30 12:36:51 -0300

    Add files via upload

---
+++ SwiftUIMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIMap
+//
+//  Created by José  Rodrigues Jr on 20/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
